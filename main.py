a72a98e1409d5f63762bc45308479a29cb7765b7
Otofuji <ericfoa@al.insper.edu.br>
2021-11-26 13:38:41 -0300

    commit de checkpoint, known-issue us-east-1 não lança instância

--- main.py
+++ main.py
@@ -4,7 +4,11 @@
 #LINHAS LONGAS | USE QUEBRA DE LINHA PARA VISUALIZAR
 
 #   Este é o projeto individual da disciplina Computação em Nuvem da Graduação em Engenharia de Computação do Insper Instituto de Ensino e Pesquisa. Para referência, o enunciado está no repositório como refs/Projeto.pdf. Referências podem ser encontradas em refs/references.txt e a elas também se somam os demais arquivos constantes no mesmo diretório.
-#   Detalhes, discussões e decisões de projetos estão documentados em código na forma de comentários como este. Cada linha de comentário equivale a um parágrafo de um texto convencional. Para fins de leitura deste material, é recomendável que esteja com a quebra de linha ativada para visualizar o conteúdo de cada parágrafo na íntegra. No VS Code, isso pode ser feito pelo atalho Alt + Z (Windows) ou Option + Z (macOS). Comentários que começam com uma tabulação, como este, o anterior e o próximo, indicam necessariamente a existência de texto explicativo sobre o que está acontecendo. Comentários sem essa tabulação são comentários normais de Python, contendo informações breves de sinalização, caso exista. Nesse caso, a seguinte notação será usada: #BLOCO para indicar o início de um bloco, e #bloco para indicar o fim desse bloco.
+#   Detalhes, discussões e decisões de projetos estão documentados em código na forma de comentários como este. Cada linha de comentário equivale a um parágrafo de um texto convencional. Para fins de leitura deste material, é recomendável que esteja com a quebra de linha ativada para visualizar o conteúdo de cada parágrafo na íntegra. No VS Code, isso pode ser feito pelo atalho Alt + Z (Windows) ou Option + Z (macOS). Comentários que começam com uma tabulação, como este, o anterior e o próximo, indicam necessariamente a existência de texto explicativo sobre o que está acontecendo. Comentários sem essa tabulação são comentários normais de Python, contendo informações breves de sinalização, caso exista. Nesse caso, a seguinte notação será usada:
+#  
+# #BLOCO para indicar o início de um bloco, e 
+# #bloco para indicar o fim desse bloco.
+
 #   O primeiro passo do projeto é a definição de uma plataforma. Para fins deste projeto, foi eleito o Boto3, que é o AWS SDK para Python. Sua documentação completa pode ser vista em https://boto3.amazonaws.com/v1/documentation/api/latest/index.html. A escolha da Amazon AWS enquanto provedor de nuvem pública é uma definição do enunciado do projeto.
 #   Existem duas formas de acessar uma instância na AWS usando boto3: low-level client e resource. O primeiro é um acesso 1:1 das chamadas de API da AWS. O último é bastante útil para quando se tem classes e objetos, pois as chamadas são relativas a esses objetos. Como não usaremos uma aplicação com classes e objetos, é mais racional usar client. 
 #   Para autorizar devidamente o acesso, foi criada uma nova política denominada AdminOhio que herda AdministratorAccess para permitir o lançamento de instâncias em us-east-2. Além disso, já estava anteriormente implantada a política AdminVirginia, também herdada de AdministratorAccess, que permite o lançamento de instâncias em us-east-1. Feitas as devidas explicações, vamos propriamente para a implantação dos primeiros passos em código para a criação e lançamento de instância, tendo por base a documentação do AWS SDK para Python, mais conhecido por Boto3. 
@@ -59,8 +63,8 @@ try:
     resp = ec2_us_east_2.describe_security_groups()
     for i in resp['SecurityGroups']:
         if(i['GroupName'] == 'SG-US-EAST-2'):
-            security_group_id = i['GroupId']
-            resp = ec2_us_east_2.delete_security_group(GroupId=security_group_id)
+            us_east_2_security_group_id = i['GroupId']
+            resp = ec2_us_east_2.delete_security_group(GroupId=us_east_2_security_group_id)
 except ClientError as e:
     print(e)
 #apaga security group existente de Ohio
@@ -72,22 +76,22 @@ try:
     resp = ec2_us_east_1.describe_security_groups()
     for i in resp['SecurityGroups']:
         if(i['GroupName'] == 'SG-US-EAST-1'):
-            security_group_id = i['GroupId']
-            resp = ec2_us_east_1.delete_security_group(GroupId=security_group_id)
+            us_east_1_security_group_id = i['GroupId']
+            resp = ec2_us_east_1.delete_security_group(GroupId=us_east_1_security_group_id)
 except ClientError as e:
     print(e)
 #apaga security group existente de Virgínia do Norte
 
-#CRIA NOVO SECURITY GROUP
+#CRIA NOVO SECURITY GROUP EM OHIO
 try:
     resp = ec2_us_east_2.create_security_group(GroupName='SG-US-EAST-2',
                                          Description='SG-US-EAST-2',
                                          VpcId=vpc_id)
-    security_group_id = resp['GroupId']
-    print('Security Group Created %s in vpc %s.' % (security_group_id, vpc_id))
+    us_east_2_security_group_id = resp['GroupId']
+    print('Security Group Created %s in vpc %s.' % (us_east_2_security_group_id, vpc_id))
 
     data = ec2_us_east_2.authorize_security_group_ingress(
-        GroupId=security_group_id,
+        GroupId=us_east_2_security_group_id,
         IpPermissions=[
             {'IpProtocol': 'tcp',
              'FromPort': 5000,
@@ -100,11 +104,35 @@ try:
         ])
 except ClientError as e:
     print(e)
-#cria novo security group
+#cria novo security group em Ohio
+
+#CRIA NOVO SECURITY GROUP EM VIRGÍNIA DO NORTE
+try:
+    resp = ec2_us_east_1.create_security_group(GroupName='SG-US-EAST-1',
+                                         Description='SG-US-EAST-1',
+                                         VpcId=vpc_id)
+    us_east_1_security_group_id = resp['GroupId']
+    print('Security Group Created %s in vpc %s.' % (us_east_1_security_group_id, vpc_id))
+
+    data = ec2_us_east_1.authorize_security_group_ingress(
+        GroupId=us_east_1_security_group_id,
+        IpPermissions=[
+            {'IpProtocol': 'tcp',
+             'FromPort': 5000,
+             'ToPort': 5000,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+            {'IpProtocol': 'tcp',
+             'FromPort': 22,
+             'ToPort': 22,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+        ])
+except ClientError as e:
+    print(e)
+#cria novo security group em Virgínia do Norte
 
 #   Agora, será criada um resource para poder lançar uma instância. Isso não é de forma alguma um desvio do que foi escrito alguns parágrafos atrás. Pesquisando sobre como lançar instâncias, vi que é necessário que se faça via resource. Porém, de acordo com a documentação do Boto3, é possível criar um client facilmente a partir de um resource posteriormente, usando conforme necessário. Este trecho foi baseado no tutorial de KGP Talkie. 
 
-#CRIA NOVA INSTÂNCIA
+#CRIA NOVA INSTÂNCIA EM OHIO
 instances = ec2_resource_us_east_2.create_instances(
     ImageId = 'ami-020db2c14939a8efb', #Ubuntu Server 18.04 LTS (HVM), SSD Volume Type 64 bits x86
     MinCount = 1,
@@ -123,4 +151,26 @@ instances = ec2_resource_us_east_2.create_instances(
     SecurityGroups = ['SG-US-EAST-2']
 
 )
-#cria nova instância
+#cria nova instância em Ohio
+
+
+#CRIA NOVA INSTÂNCIA EM EM VIRGÍNIA DO NORTE
+instances = ec2_resource_us_east_1.create_instances(
+    ImageId = 'ami-0279c3b3186e54acd', #Ubuntu Server 18.04 LTS (HVM), SSD Volume Type 64 bits x86
+    MinCount = 1,
+    MaxCount = 1,
+    InstanceType = 't2.micro',
+    KeyName = 'KeyName',
+    BlockDeviceMappings = [
+        {
+            'DeviceName' : "/dev/xvda",
+            'Ebs' :{
+                'DeleteOnTermination': False,
+                'VolumeSize': 20
+            }
+        }
+    ],
+    SecurityGroups = ['SG-US-EAST-1']
+
+)
+#cria nova instância em Virgínia do Norte
