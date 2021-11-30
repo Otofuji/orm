bc03ea1323da70a22f0649b3f23a02dfc44d18a4
Otofuji <ericfoa@al.insper.edu.br>
2021-11-30 12:00:52 -0300

    checkpoint

--- main.py
+++ main.py
@@ -17,7 +17,7 @@
 #   AVISO DISCIPLINAR: A um observador externo e conhecedor das diretrizes gerais de programação do Insper, pode suscitar dúvidas quanto à consulta feita ao trabalho elaborado por franciol. Todavia, cabe ressaltar que essa visualização foi feita dentro de premissas permitidas e legais, diante do objetivo de aprendizagem para este projeto, que envolve uma etapa de pesquisa tanto em documentação quanto em projetos similares. Não por interpretação minha, mas por explícita autorização tanto do professor da disciplina quanto do autor do projeto anterior e, além disso, com a explícita inclusão dos créditos a todas as referências consultadas. Note que a pasta de referências contém extensas documentações e linhas de referências em references.txt. Todo esse material foi usado para embasar este projeto e de forma nenhuma constitui plágio ou infração às diretrizes de integridade intelectual do Insper. Antes de proceder à visualização desse material, foi discutido com o professor em detalhes sobre essa autorização, definindo o que está dentro da proposta do projeto e é considerado aceitável ou recomendável. Portanto, declaro que tudo está dentro das regras como deveria estar e que não foi cometida nenhuma desonestidade intelectual no processo de elaboração deste projeto, submetido à avaliação para a disciplina Computação em Nuvem em 3 de dezembro de 2021. Caso você seja um aluno lendo isto para fazer o projeto de uma disciplina, converse com seu professor antes de prosseguir a leitura para ter certeza que está tudo bem estar lendo isso. Caso contrário, não prossiga com a leitura.
 #   Até 26/11/2021, estava tendando primeiro criar os SGs para depois verificar as instâncias. Porém, me deparei com um problema inesperado. Ao rodar o programa pela segunda vez, ele não conseguia apagar o SG existente. De acordo com https://stackoverflow.com/questions/61236712/unable-to-delete-security-group-an-error-occurred-dependencyviolation-when-ca, isso se deve à existência de uma instância que foi criada na última vez em que foi rodado. Para contornar isso, primeiro apagarei as instâncias existentes, usando uma adaptação prática do que consta na documentação do Boto3 https://boto3.amazonaws.com/v1/documentation/api/latest/guide/migrationec2.html. Porém, após isso, o problema persistiu. Em conversas com Henrique Mualem, ele sugeriu uma forma diferente de realizar o mesmo procedimento. Primeiro, pega-se as instâncias usando ec2.describe_instances() ao invés de ec2.instances.filter() como havia feito inicialmente, e então filtrar os dados desse dicionário. Além disso, não estava aguardando que a instância termine, pois era necessário aguardar e, por isso, estava dando erro. Passei então a usar esse método sugerido por ele a partir de 29/11/2021, que consiste, além do mencionado, no seguinte: descrever as instâncias e SGs existentes e então deletar as instâncias usando o SG que estamos querendo usar (assim, caso haja outra instância para alguma outra coisa, este código não atrapalha ela). Após fazer isso, aguardar cinco segundos e veificar se a instância foi encerrada, e caso não aguardar mais cinco segundos. Após confirmado o encerramento da instância, apaga-se o SG de interesse e então cria-se ele. Após, autorizar o Security Group e por fim criar a instância na forma como estava criando ele inicialmente seguindo o modo sugerido pelo KGP Talkie no seu vídeo tutorial.
 #   Será criada um resource para poder lançar uma instância. Isso não é de forma alguma um desvio do que foi escrito alguns parágrafos atrás. Pesquisando sobre como lançar instâncias, vi que é necessário que se faça via resource. Porém, de acordo com a documentação do Boto3, é possível criar um client facilmente a partir de um resource posteriormente, usando conforme necessário. Este trecho foi baseado no tutorial de KGP Talkie. 
-#   Até 29/11/2021 (239443089bbd8e8d72f14f3c5bf6ab8303fe90e9), as operações de apagar instâncias e security groups existentes e então criar security group e instância funcionam perfeitamente para Ohio, mas por um motivo que a lógica não é capaz de explicar, não funciona para a Virgínia do Norte, embora seja empregado exatamente o mesmo código da mesma forma. Isso não faz sentido algum. Conversando com colegas que passaram por essa parte, não fizeram nada de diferente, exceto pelo fato de terem usado funções ao invés de escrever o código hardcoded de forma sequencial como se fosse um programa Assembly como fiz até o commit 239443089bbd8e8d72f14f3c5bf6ab8303fe90e9. Optei por então refatorar o código, criando uma única função que dispara instâncias após verificar e apagar SG e instâncias anteriores, e chamando ela depois duas vezes, uma para cada região. Por algum motivo que a lógica da computação não é capaz de explicar, isso funcionou. Não havia nenhum pequeno erro de digitação na versão anterior, então realmente isso é algo sem explicação racional.
+#   Até 29/11/2021 (239443089bbd8e8d72f14f3c5bf6ab8303fe90e9), as operações de apagar instâncias e security groups existentes e então criar security group e instância funcionam perfeitamente para Ohio, mas por um motivo que a lógica não é capaz de explicar, não funciona para a Virgínia do Norte, embora seja empregado exatamente o mesmo código da mesma forma. Isso não faz sentido algum. Conversando com colegas que passaram por essa parte, não fizeram nada de diferente, exceto pelo fato de terem usado funções ao invés de escrever o código hardcoded de forma sequencial como se fosse um programa Assembly como fiz até o commit 239443089bbd8e8d72f14f3c5bf6ab8303fe90e9. Optei por então refatorar o código, criando uma única função que dispara instâncias após verificar e apagar SG e instâncias anteriores, e chamando ela depois duas vezes, uma para cada região.
 #    Em 30/11/2021, porém, reparei que essa arquitetura não será adequada para as próximas etapas do projeto. Isso porque se a criação de instâncias se dá dentro de uma função, devido às variáveis locais será difícil acessar os clientes do boto3. Refletindo sobre o programa, decidi refatorar novamente, e agora separando as regiões por scripts hardcoded. Este script, o main, será responsável por orquestrar os deploys, sendo que o apagamento de instâncias existentes, apagamento de security groups existentes e lançamento de novas instâncias em cada região serão feitos em scripts separados. 
 #   Decidi por uma nova refatoração no mesmo dia logo em seguida, trazendo tudo para o mesmo script, mas separando por funções. Assim, o que tem que ser feito numa região é uma função, e o que tem que ser feito na outra região é outra função. Isso resolve o problema mencionado acima.
 
@@ -29,6 +29,21 @@ import boto3
 from botocore.exceptions import ClientError
 import time
 #imports
+
+def execute_commands_on_linux_instances(client, commands, instance_ids): #Função extraída na íntegra de https://stackoverflow.com/questions/42645196/how-to-ssh-and-run-commands-in-ec2-using-boto3
+    """Runs commands on remote linux instances
+    :param client: a boto/boto3 ssm client
+    :param commands: a list of strings, each one a command to execute on the instances
+    :param instance_ids: a list of instance_id strings, of the instances on which to execute the command
+    :return: the response from the send_command function (check the boto3 docs for ssm client.send_command() )
+    """
+
+    resp = client.send_command(
+        DocumentName="AWS-RunShellScript", # One of AWS' preconfigured documents
+        Parameters={'commands': commands},
+        InstanceIds=instance_ids,
+    )
+    return resp
 
 def deploy_us_east_2():
     
@@ -115,7 +130,19 @@ def deploy_us_east_2():
         print(us_east_2_security_group_id)
         print('Security Group Created %s in vpc %s.' % (us_east_2_security_group_id, vpc_id))
 
-        data = ec2_us_east_2.authorize_security_group_ingress(
+        data_in = ec2_us_east_2.authorize_security_group_ingress(
+            GroupId=us_east_2_security_group_id,
+            IpPermissions=[
+                {'IpProtocol': 'tcp',
+                'FromPort': 5000,
+                'ToPort': 5000,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                {'IpProtocol': 'tcp',
+                'FromPort': 22,
+                'ToPort': 22,
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+            ])
+        data_out = ec2_us_east_2.authorize_security_group_egress(
             GroupId=us_east_2_security_group_id,
             IpPermissions=[
                 {'IpProtocol': 'tcp',
@@ -154,6 +181,47 @@ def deploy_us_east_2():
     )
     print("Criou nova instância em Ohio")
     #cria nova instância em Ohio
+
+
+    #CONFIGURA A NOVA INSTÂNCIA USANDO https://stackoverflow.com/questions/42645196/how-to-ssh-and-run-commands-in-ec2-using-boto3
+    print("Configurando a nova instância em Ohio")
+    ssm_client = boto3.client('ssm')
+
+
+
+
+
+    #################################
+
+    instances = ec2_us_east_2.describe_instances()
+    len_instances = len(instances['Reservations'])
+    for n in range(len_instances):
+        try:
+            instance_security_group = instances['Reservations'][n]['Instances'][0]['NetworkInterfaces'][0]['Groups'][0]['GroupName']
+            if instance_security_group == 'SG-US-EAST-2':
+                instanceid = instances['Reservations'][n]['Instances'][0]['InstanceId']
+        except:
+            pass
+
+    #################################
+
+
+    instance_id = []
+    instance_id.append(instanceid)
+    command = ['sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y']
+    loading = True
+    while loading:
+
+        try:
+            execute_commands_on_linux_instances(ssm_client, command, instance_id)
+            loading = False
+
+        except: 
+            print("    Aguarde inicialização da instância - não desligue o programa")
+            loading = True
+            time.sleep(30)
+    print("Instância rodando, configurando...")
+    #configura a nova instância usando https://stackoverflow.com/questions/42645196/how-to-ssh-and-run-commands-in-ec2-using-boto3
 
 def deploy_us_east_1():
     finishing = False
